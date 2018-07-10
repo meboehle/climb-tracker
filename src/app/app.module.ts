e9a9c688c7ffc8397edbe2840e395a0f9a7481f1
meboehle <meboehle@ncsu.edu>
2018-07-09 22:49:17 -0400

    imported angular material: icon,card,button,menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material'; 
+import { MatButtonModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+
 import { AppComponent } from './app.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { ClimbOfDayComponent } from './climb-of-day/climb-of-day.component';
+import { ClimbsByRegionComponent } from './climbs-by-region/climbs-by-region.component';
+import { ClimbsMapComponent } from './climbs-map/climbs-map.component';
+import { ClimbCardComponent } from './climb-card/climb-card.component';
+import { LinkFooterComponent } from './link-footer/link-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeaderComponent,
+    ClimbOfDayComponent,
+    ClimbsByRegionComponent,
+    ClimbsMapComponent,
+    ClimbCardComponent,
+    LinkFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
