2ec3325e372b6e35242a8c2969dfac4dc301d80b
meboehle <meboehle@ncsu.edu>
2018-07-21 09:58:55 -0400

    changed title&menu item names + routerlinks to the menu items

--- src/app/nav-header/nav-header.component.ts
+++ src/app/nav-header/nav-header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavHeaderComponent implements OnInit {
 
-  title = "Climbs";
+  title = "Rock Climbing Guide";
 
   constructor() { }
 
