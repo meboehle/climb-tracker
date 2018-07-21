36bb6f5950dc4f117c405277ab5a8a903c4b4c10
meboehle <meboehle@ncsu.edu>
2018-07-21 09:50:02 -0400

    + forms,routing,new components - 3 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material'; 
 import { MatButtonModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
 import { ClimbOfDayComponent } from './climb-of-day/climb-of-day.component';
-import { ClimbsByRegionComponent } from './climbs-by-region/climbs-by-region.component';
-import { ClimbsMapComponent } from './climbs-map/climbs-map.component';
 import { ClimbCardComponent } from './climb-card/climb-card.component';
-import { LinkFooterComponent } from './link-footer/link-footer.component';
+import { ClimbsListComponent } from './climbs-list/climbs-list.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { BannerComponent } from './banner/banner.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavHeaderComponent,
     ClimbOfDayComponent,
-    ClimbsByRegionComponent,
-    ClimbsMapComponent,
     ClimbCardComponent,
-    LinkFooterComponent
+    ClimbsListComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +33,10 @@ import { LinkFooterComponent } from './link-footer/link-footer.component';
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
