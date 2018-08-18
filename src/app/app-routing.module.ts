64f9a752783022fa8dc4143c6352dab888095011
meboehle <meboehle@ncsu.edu>
2018-08-18 10:56:43 -0400

    + new route to shopItems * editedthe home path to route to climbCard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClimbsListComponent } from './climbs-list/climbs-list.component';
-import { ClimbOfDayComponent } from './climb-of-day/climb-of-day.component';
+import { ClimbCardComponent } from './climb-card/climb-card.component';
+import { ShopItemsComponent } from './shop-items/shop-items.component';
 
 
-//Fix the routes and components
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'my-climbs-list', component: ClimbsListComponent },
-  { path: 'home', component: ClimbOfDayComponent }
+  { path: 'home', component: ClimbCardComponent },
+  { path: 'climb-shop', component: ShopItemsComponent }
 ];
 
 @NgModule({
