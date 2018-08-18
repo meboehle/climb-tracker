eaa2fc3ec411239c0254c273919473e4db1d274a
meboehle <meboehle@ncsu.edu>
2018-08-18 11:16:57 -0400

    switched to a bootstrap navbar withh 3 router links

--- src/app/nav-header/nav-header.component.ts
+++ src/app/nav-header/nav-header.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'nav-header',
+  selector: 'app-nav-header',
   templateUrl: './nav-header.component.html',
   styleUrls: ['./nav-header.component.scss']
 })
 export class NavHeaderComponent implements OnInit {
 
-  title = "Rock Climbing Guide";
+  title = 'Rock Climbing Guide';
 
   constructor() { }
 
