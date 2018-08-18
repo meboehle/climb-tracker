0d869c430fd2879614f7e9ee1e4ef27110e58a80
meboehle <meboehle@ncsu.edu>
2018-08-18 10:58:01 -0400

    + boolean inList property - rating property

--- src/app/mock-climbs.ts
+++ src/app/mock-climbs.ts
@@ -1,10 +1,10 @@
 import { Climb } from './climb';
 
 export const CLIMBS: Climb[] = [
-    { name: 'White Rastafarian', grade: 'V2', location: 'Joshua Tree, California', rating: '3.9/5' },
-    { name: 'Jedi Mind Tricks', grade: 'V4', location: 'Bishop, California', rating: '3.9/5' },
-    { name: 'Moonshine Roof', grade: 'V4', location: 'Hueco Tanks, Texas', rating: '4/5' },
-    { name: 'Golden Shower', grade: 'V5', location: 'Rocktown Georgia', rating: '3.9/5' },
-    { name: 'Epinephrine', grade: '5.9', location: 'Red Rock, Nevada', rating: '3.9/5' },
-    { name: 'Astroman', grade: '5.11c', location: 'Yosemtie, California', rating: '4/5' }
-]
+    { name: 'White Rastafarian', grade: 'V2', location: 'Joshua Tree, California', inMyList: false },
+    { name: 'Jedi Mind Tricks', grade: 'V4', location: 'Bishop, California', inMyList: false },
+    { name: 'Moonshine Roof', grade: 'V4', location: 'Hueco Tanks, Texas', inMyList: false },
+    { name: 'Golden Shower', grade: 'V5', location: 'Rocktown Georgia', inMyList: false },
+    { name: 'Epinephrine', grade: '5.9', location: 'Red Rock, Nevada', inMyList: false },
+    { name: 'Astroman', grade: '5.11c', location: 'Yosemtie, California', inMyList: false }
+];
